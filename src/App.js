79f384af5788db7391463cf202c842ec57317bac
Joey Yu <joey@itsjoeoui.com>
2022-03-16 09:43:29 -0400

    Connect toggleReminder with the backend
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- src/App.js
+++ src/App.js
@@ -25,8 +25,16 @@ function App() {
     return data;
   };
 
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:2333/tasks/${id}`);
+    const data = await res.json();
+
+    console.log(data);
+    return data;
+  };
+
   const addTask = async (task) => {
-    const res = await fetch(`http://localhost:2333/tasks/`, {
+    const res = await fetch("http://localhost:2333/tasks/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(task),
@@ -44,14 +52,23 @@ function App() {
     console.log(`[${id}] Task Deleted`);
   };
 
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:2333/tasks/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(updatedTask),
+    });
+    const data = await res.json();
+
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
-
-    console.log(`[${id}] Reminder Toggled`);
+    console.log(`[${data.id}] Reminder Toggled`);
   };
 
   return (

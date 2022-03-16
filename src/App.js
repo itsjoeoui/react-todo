1ef9abc0115731a2ce1b94f5f90935dd7359b140
Joey Yu <joey@itsjoeoui.com>
2022-03-16 10:06:57 -0400

    Explore react-router-dom
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(true);
@@ -72,20 +75,39 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <Header
-        onAdd={() => {
-          setShowAddTask(!showAddTask);
-        }}
-        showAddTask={showAddTask}
-      />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-      ) : (
-        "No Tasks!"
-      )}
-    </div>
+    <Router>
+      <div className="container">
+        <Header
+          onAdd={() => {
+            setShowAddTask(!showAddTask);
+          }}
+          showAddTask={showAddTask}
+        />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                {" "}
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  "No Tasks!"
+                )}
+              </>
+            }
+          />
+          <Route path="about" element={<About />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,23 @@
 // impt
 import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
 import Button from "./Button";
 
 // rafce
 const Header = ({ title, onAdd, showAddTask }) => {
+  const location = useLocation();
+
   return (
     <header>
       <h1 className="header">
         {title}
-        <Button
-          color={showAddTask ? "red" : "green"}
-          text={showAddTask ? "Close" : "Add"}
-          onAdd={onAdd}
-        />
+        {location.pathname === "/" && (
+          <Button
+            color={showAddTask ? "red" : "green"}
+            text={showAddTask ? "Close" : "Add"}
+            onAdd={onAdd}
+          />
+        )}
       </h1>
     </header>
   );
